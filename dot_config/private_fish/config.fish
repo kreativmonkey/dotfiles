334964b365d48478d1cc4ad0a8f70a96704b10d3
Sebastian Preisner <preisner@puzzle-itc.de>
2022-07-15 06:11:08 +0200

    Update dot_config/private_fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-mcfly init fish | source
-zoxide init --cmd cd fish | source
+if test -e /usr/bin/mcfly
+  mcfly init fish | source
+end
+# Includes Zoxide for fast cd commands
+if test -e /usr/bin/zoxide
+  zoxide init fish | source
+end
