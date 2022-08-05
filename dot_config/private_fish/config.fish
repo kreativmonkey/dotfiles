95910038ae754945b04880159d5d2385ffa9c1f8
Sebastian Preisner <preisner@puzzle-itc.de>
2022-08-05 14:50:18 +0200

    Update dot_config/private_fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,7 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    #set -g FZF_DEFAULT_COMMAND=
-    set -g FZF_DEFAULT_OPTS "--preview 'cat {}'"
 end
 if test -e /usr/bin/mcfly
   mcfly init fish | source
@@ -10,4 +8,6 @@ end
 if test -e /usr/bin/zoxide
   zoxide init fish | source
 end
-
+if test -e /usr/bin/fzf
+  fzf_configure_bindings --history=\cs
+end
